b3b0288cd3716ead0014f29fd82e78f984b726d7
User <afs1424@gmail.com>
2022-03-15 17:08:39 +0300

    forgotten commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
-import first from "./images/2.png"
-import second from "./images/7.jpg"
+import logo from "./images/logo.png"
+import im2 from "./images/2.png"
+import im3 from "./images/3.png"
+import im4 from "./images/4.png"
+import im5 from "./images/5.png"
+import im6 from "./images/6.png"
+import {CCarousel, CCarouselCaption, CCarouselItem, CImage} from '@coreui/react'
+import '@coreui/coreui/dist/css/coreui.min.css'
+
 
 function App() {
-  return (
+
+    return (
     <div className="App">
         <div className="App-header-outer">
             <header className="App-header">
@@ -13,8 +21,8 @@ function App() {
                     </div>
                 </div>
                 <div className="App-header-container">
-                    <div className="App-header-img">
-                        <img src={second} />
+                    <div className="App-header-text">
+                        <img width="40px" height="40px" src={logo} />
                     </div>
                 </div>
             </header>
@@ -38,6 +46,47 @@ function App() {
                 <div className="App-menu-text"></div>
             </header>
         </div>
+        <div>
+            <CCarousel controls indicators>
+                <CCarouselItem>
+                    <CImage className="d-inline-block w-50" src={im2} alt="slide 1" />
+                    <CCarouselCaption className="d-none d-md-block">
+                        <h5>First slide label</h5>
+                        <p>Some representative placeholder content for the first slide.</p>
+                    </CCarouselCaption>
+                </CCarouselItem>
+                <CCarouselItem>
+                    <CImage className="d-inline-block w-50" src={im3} alt="slide 2" />
+                    <CCarouselCaption className="d-none d-md-block">
+                        <h5>Second slide label</h5>
+                        <p>Some representative placeholder content for the first slide.</p>
+                    </CCarouselCaption>
+                </CCarouselItem>
+                <CCarouselItem>
+                    <CImage className="d-inline-block w-50" src={im4} alt="slide 3" />
+                    <CCarouselCaption className="d-none d-md-block">
+                        <h5>Third slide label</h5>
+                        <p>Some representative placeholder content for the first slide.</p>
+                    </CCarouselCaption>
+                </CCarouselItem>
+
+                <CCarouselItem>
+                    <CImage className="d-inline-block w-50" src={im5} alt="slide 3" />
+                    <CCarouselCaption className="d-none d-md-block">
+                        <h5>Third slide label</h5>
+                        <p>Some representative placeholder content for the first slide.</p>
+                    </CCarouselCaption>
+                </CCarouselItem>
+
+                <CCarouselItem>
+                    <CImage className="d-inline-block w-50" src={im6} alt="slide 3" />
+                    <CCarouselCaption className="d-none d-md-block">
+                        <h5>Third slide label</h5>
+                        <p>Some representative placeholder content for the first slide.</p>
+                    </CCarouselCaption>
+                </CCarouselItem>
+            </CCarousel>
+        </div>
         <div className="Containers">
             <div className="Cards-Container">
                 <div className="Card">
@@ -49,28 +98,29 @@ function App() {
                     </div>
                 </div>
                 <div className="Card">
-                    Вы спросите один вопрос? Зачем нам нужно делиться с вами методикой, которая приносит деньги? Дело в том, что выкладывая торговые стратегии и их результаты публично, мы чувствуем большую ответственность в принятии решений и получаем мотивацию для улучшения показателей эффективности нашей торговли. Для нас этот проект служит в главной степени стимулом к получению большей прибыли, а плата за подписку на канале является приятным бонусом для нас) это мы скрывать не будем)
+                    <p>Вы спросите один вопрос? Зачем нам нужно делиться с вами методикой, которая приносит деньги? Дело в том, что выкладывая торговые стратегии и их результаты публично, мы чувствуем большую ответственность в принятии решений и получаем мотивацию для улучшения показателей эффективности нашей торговли. Для нас этот проект служит в главной степени стимулом к получению большей прибыли, а плата за подписку на канале является приятным бонусом для нас) это мы скрывать не будем)</p>
 
 
                 </div>
                 <div className="Card">
-                    Подписка на наш телеграмм канал подойдет, как для трейдера новичка, который не знает с чего начать изучение основ биржевой торговли, так и для профессионала, ищущего сигналы для входа в сделку и интересные паттерны на графиках. Мы расскажем, с чего лучше начать или как развить уже имеющиеся у тебя навыки трейдера и идти в ногу со временем и рынком.
-                    В нашем платном телеграм канале ты получишь:
-                    ⦁	Торговые стратегии по криптовалютам на спотовых и срочных рынках.
-                    ⦁	Советы по торговле криптовалютами
-                    ⦁	Основные принципы работы на бирже (ввод, вывод средств, размещение ордеров и т.д.)
-                    ⦁	Результаты поездок с блокчейн форумов и приглашения на закрытые встречи трейдеров нашего канала
-                    ⦁	Пособия по применении технического анализа для криптовалютного рынка
-                    ⦁	Пособия по использованию “on-chain” анализа для торговли (Glassnode)
-
+                    <p>Подписка на наш телеграмм канал подойдет, как для трейдера новичка, который не знает с чего начать изучение основ биржевой торговли, так и для профессионала, ищущего сигналы для входа в сделку и интересные паттерны на графиках. Мы расскажем, с чего лучше начать или как развить уже имеющиеся у тебя навыки трейдера и идти в ногу со временем и рынком.</p>
+                    <p>В нашем платном телеграм канале ты получишь:</p>
+                    <p>⦁	Торговые стратегии по криптовалютам на спотовых и срочных рынках.</p>
+                    <p>⦁	Советы по торговле криптовалютами</p>
+                    <p>⦁	Основные принципы работы на бирже (ввод, вывод средств, размещение ордеров и т.д.)</p>
+                    <p>⦁	Результаты поездок с блокчейн форумов и приглашения на закрытые встречи трейдеров нашего канала</p>
+                    <p>⦁	Пособия по применении технического анализа для криптовалютного рынка</p>
+                    <p>⦁	Пособия по использованию “on-chain” анализа для торговли (Glassnode)</p>
                 </div>
                 <div className="Card">
-                    Если хочешь окунуться в мир современной торговли и научиться вкладывать свои деньги правильно, обращайся к нам по следующим ссылкам:
-                    Здесь ты найдешь всю необходимую информацию и поддержку твоих идей.
-                    P.S. Поспеши, биткойн вновь обновил свой исторический максимум ;)
-
+                    <p>Если хочешь окунуться в мир современной торговли и научиться вкладывать свои деньги правильно, обращайся к нам по следующим ссылкам:</p>
+                    <p>Здесь ты найдешь всю необходимую информацию и поддержку твоих идей.</p>
+                    <p>P.S. Поспеши, биткойн вновь обновил свой исторический максимум ;)</p>
                 </div>
             </div>
+        </div>
+        <div className="Footer">
+
         </div>
     </div>
   );
